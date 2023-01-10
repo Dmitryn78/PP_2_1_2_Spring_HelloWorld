1e0d20a398aaad13ee5c614c010452a04796e929
Dmitry Novikov <Dmitryn78@yandex.ru>
2023-01-10 18:19:33 +0300

    выполнил задание

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,21 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.concurrent.Callable;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("bean & bean1 equal by reference = " + (bean1 == bean2));
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("cat1 & cat2 equal by reference = " + (cat1 == cat2));
     }
 }
